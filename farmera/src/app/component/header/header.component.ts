4b57ef53760e3cf7c54c29a4fd3e979d027b571d
Josue <jgutierrez@reprintsdesk.com>
2019-10-01 16:55:59 -0500

    feature: Adding Action Sheet

--- farmera/src/app/component/header/header.component.ts
+++ farmera/src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController} from '@ionic/angular';
+import { PigService } from '../../services/pigs.service';
 
 @Component({
   selector: 'farmera-header',
@@ -7,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private actionSheetCtrl: ActionSheetController,
+    private pigService: PigService,
+    ) { }
+
+  async presentActions() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header : 'Select an action',
+      buttons: [{
+        text: 'Add pigs',
+        icon: 'add',
+        handler: this.pigService.addPig
+      },{
+        text: 'Move pigs',
+        icon: 'move',
+        handler: this.pigService.movePig
+      },{
+        text: 'Sale pigs',
+        icon: 'card',
+        handler: this.pigService.salePig
+      }]
+    });
+
+    await actionSheet.present();
+
+  }
 
   ngOnInit() {}
 
---
+++ farmera/src/app/services/pigs.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PigService {
+  constructor() {}
+  addPig() {
+    console.log('Add pig 🐷');
+  }
+  movePig(){
+    console.log('Move pig 🚛');
+  }
+  salePig(){
+    console.log('Sale pig 💰');
+  }
+
+}
